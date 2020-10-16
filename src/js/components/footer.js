903318ebd755e2e6694ab0b4599dc1d1c4516086
moldovanjason <moldovanjason@gmail.com>
2020-10-16 02:00:59 +0000

    M

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,3 +1,11 @@
 import React, { Component } from "react";
+import { Link } from "react-router";
 
-export const Footer = () => <footer className="footer mt-auto py-3 text-center" />;
+const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center">
+		<div>For more info about Star Wars go to:</div>
+		<Link to="Starwars.com" />
+	</footer>
+);
+
+export default Footer;
